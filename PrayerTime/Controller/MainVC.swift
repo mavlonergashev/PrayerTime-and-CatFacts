4ed54b63d7fc43a17ceb7082ba4e6652faf9332c
mavlonergashev <mavlik.exe@gmail.com>
2022-04-01 12:22:52 +0500

    IBOutlets set

--- PrayerTime/Controller/MainVC.swift
+++ PrayerTime/Controller/MainVC.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    @IBOutlet weak var cityNameLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var fajrTime: UILabel!
+    @IBOutlet weak var sunriseTime: UILabel!
+    @IBOutlet weak var dhuhrTime: UILabel!
+    @IBOutlet weak var asrTime: UILabel!
+    @IBOutlet weak var maghribTime: UILabel!
+    @IBOutlet weak var ishaaTime: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
